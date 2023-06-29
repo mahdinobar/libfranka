69c8a2c2472dc1e4f6aef2d6a3cf0d8d0578ec60
mnobar <nobar.mahdi@gmail.com>
2023-06-29 12:38:54 +0200

    added template target and source file

---
+++ examples/tmp_step_response.cpp
@@ -0,0 +1,47 @@
+//
+// Created by mahdi on 29.06.23.
+//
+///
+// Created by mahdi on 29.06.23.
+//
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+#include <cmath>
+#include <iostream>
+
+#include <franka/exception.h>
+#include <franka/robot.h>
+
+#include "examples_common.h"
+
+/**
+ * @example tmp_step_response.cpp
+ * An example showing how to generate a joint position motion.
+ *
+ * @warning Before executing this example, make sure there is enough space in front of the robot.
+ */
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
+    return -1;
+  }
+  try {
+    franka::Robot robot(argv[1]);
+    setDefaultBehavior(robot);
+
+    // First move the robot to a suitable joint configuration
+    std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
+    MotionGenerator motion_generator(0.3, q_goal);
+    std::cout << "WARNING: This example will move the robot! "
+              << "Please make sure to have the user stop button at hand!" << std::endl
+              << "Press Enter to continue..." << std::endl;
+    std::cin.ignore();
+    robot.control(motion_generator);
+    std::cout << "Finished moving to initial joint configuration." << std::endl;
+  } catch (const franka::Exception& e) {
+    std::cout << e.what() << std::endl;
+    return -1;
+  }
+  return 0;
+}
